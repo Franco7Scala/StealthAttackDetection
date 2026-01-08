48c9c37d564cb751687626820bc9782c0de59d62
Francesco Scala <franco7scala@gmail.com>
2026-01-08 15:49:36 +0100

    Update dataset_loader.py

--- src/dataset/dataset_loader.py
+++ src/dataset/dataset_loader.py
@@ -5,15 +5,21 @@ from src.dataset._cobalt_loader import load_cobalt_dataset
 
 def load_dataset(attack_type, n_train, n_validation, n_test, device):
     if attack_type.lower() == "slowdos":
-        dataset = load_slowdos_dataset(n_train, n_validation, n_test)
+        dataset = load_slowdos_dataset()
 
     elif attack_type.lower() == "covert":
-        dataset = load_covert_dataset(n_train, n_validation, n_test)
+        dataset = load_covert_dataset()
 
     elif attack_type.lower() == "cobalt":
-        dataset = load_cobalt_dataset(n_train, n_validation, n_test)
+        dataset = load_cobalt_dataset()
 
     else:
         raise Exception(f"Unknown dataset for attack type '{attack_type}'!")
 
+    dataset = _split_dataset(dataset, n_train, n_validation, n_test)
     return dataset.to(device)
+
+
+def _split_dataset(dataset, n_train, n_validation, n_test):
+    pass
+
