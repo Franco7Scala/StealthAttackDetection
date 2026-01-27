3858d8b7cf9f0887ef24f0455e35b2db6105ddc7
Francesco Scala <franco7scala@gmail.com>
2026-01-27 17:04:45 +0100

    fixed args

--- src/support/arguments.py
+++ src/support/arguments.py
@@ -1,6 +1,8 @@
 import argparse
 import torch
 
+from src.support.utils import str2bool
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Parse arguments for anomaly detection experiments.")
@@ -10,19 +12,17 @@ def parse_arguments():
     parser.add_argument("--train-normal-ratio", type=float, default=0.5, help="Fraction of normal data to use for training.")
     parser.add_argument("--b-max", type=int, default=80, help="Maximum fraction of attack data to consider for the budget.")
     parser.add_argument("--n-train-attacks", type=int, default=5, help="Number of attack samples to include in the few-shot training set.")
-    parser.add_argument("--apply-normalization", type=bool, default=False, help="Enable data normalization.")
+    parser.add_argument("--apply-normalization", type=str2bool, default=False, help="Enable data normalization.")
     parser.add_argument("--batch-size", type=int, default=32, help="Batch size for the DataLoaders.")
     parser.add_argument("--z-dim", type=int, default=8, help="Latent code dimension for ARN Generator.")
     parser.add_argument("--nf-out", type=int, default=8, help="Number of neurons for ARN Generator.")
     parser.add_argument("--n_runs", type=int, default=1, help="Number of experimental runs.")
-    parser.add_argument("--n_exps",type=str,default="freeze_discriminator_generator")
+    parser.add_argument("--n_exps",type=str, default="freeze_discriminator_generator")
     parser.add_argument("--num_epochs", type=int, default=50, help="Number of epochs to train ARN.")
     parser.add_argument("--lr_D", type=float, default=0.0001, help="Learning rate for ARN Discriminator.")
     parser.add_argument("--lr_G", type=float, default=0.001, help="Learning rate for ARN Generator.")
     parser.add_argument("--SAVE_FOLDER", type=str, default="./saved", help="Folder for saving models.")
     parser.add_argument("--nout", type=int, default=16, help="Number of neurons of the feature extractor for ARN Discriminator.")
-    parser.add_argument("--nc_out", type=int, default=4,
-                        help="Number of neurons of the classifier for ARN Discriminator.")
+    parser.add_argument("--nc_out", type=int, default=4, help="Number of neurons of the classifier for ARN Discriminator.")
     parser.add_argument("--n_epochs_cpvae", type=int, default=16, help="Number of epochs to train CPVAE Model.")
-
     return parser.parse_args()
--- src/support/utils.py
+++ src/support/utils.py
@@ -20,3 +20,17 @@ def compute_scale_pos_weight(y_train_np):
     n_pos = (y_train_np == 1).sum()
     n_neg = (y_train_np == 0).sum()
     return n_neg / n_pos
+
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+
+    if v.lower() in ("yes", "true", "t", "y", "1"):
+        return True
+
+    elif v.lower() in ("no", "false", "f", "n", "0"):
+        return False
+
+    else:
+        raise Exception("Boolean value expected.")
