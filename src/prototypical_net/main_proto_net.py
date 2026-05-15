a8bae4160ca0f5a94effb4bef59f9a3a9c268452
bernardo9830 <bernardokr98@gmail.com>
2026-05-15 12:47:22 +0000

    Prototypal runs

--- src/prototypical_net/main_proto_net.py
+++ src/prototypical_net/main_proto_net.py
@@ -1,8 +1,9 @@
 import torch
+import time 
 import os
 import numpy
 import torch.nn as nn
-
+import pandas as pd
 from src.dataset.dataset_loader import load_dataset, get_dataloaders
 from src.model.predictive_model import AverageMeter
 from src.prototypical_net.trainer_proto import train_proto
@@ -10,7 +11,7 @@ from src.support.arguments import parse_arguments
 from src.support.utils import set_reproducibility, print_args
 from src.arn.model import Generator
 from sklearn.cluster import KMeans
-from sklearn.metrics import average_precision_score, f1_score, confusion_matrix, recall_score, accuracy_score
+from sklearn.metrics import average_precision_score, f1_score, confusion_matrix, recall_score, accuracy_score, roc_auc_score,    precision_score
 
 
 if __name__ == "__main__":
@@ -31,6 +32,7 @@ if __name__ == "__main__":
     print(f"Running on {device}...")
     x_train_unsupervised = x_train_unsupervised.to(device)
     input_size = x_train_unsupervised.shape[1]
+    start_time_total = time.time()
     if use_vae_pretrained:
         name_VAE_model = f"ARN_Generator_{attack_type}_0.ckpt"
         path_VAE_model = os.path.join(args.SAVE_FOLDER, "models", name_VAE_model)
@@ -59,10 +61,12 @@ if __name__ == "__main__":
     z_norm_np = z_norm.cpu().numpy()
     #numpy.random.shuffle(z_norm_np)
     #z_norm_np = z_norm_np[:100]
-
+    
+   
     print(f"Clustering normal traffic in {num_normal_prototypes} prototypes...")
     kmeans = KMeans(n_clusters=num_normal_prototypes, random_state=args.seed, n_init=10)
     kmeans.fit(z_norm_np)
+    training_time_total = time.time() - start_time_total
     norm_prototypes = torch.tensor(kmeans.cluster_centers_, dtype=torch.float32).to(device)
 
     print("Calculating prototype of the attack...")
@@ -112,7 +116,34 @@ if __name__ == "__main__":
     far = fp / (fp + tn)
     acc = accuracy_am.avg
     recalls_per_class = recall_score(y_true, y_pred, average=None)
+    precision = precision_score(y_true, y_pred, average='macro')
+    recall = recall_score(y_true, y_pred, average='macro')
     g_mean = numpy.prod(recalls_per_class) ** (1 / len(recalls_per_class))
+    auc = roc_auc_score(y_true, y_scores)
+
+    row = {
+    "run_id": args.n_runs,   
+    "attack_type": args.attack_type,
+    "model": "prototypical",
+    "accuracy_last": acc,
+    "precision_last": precision,
+    "recall_last": recall,
+    "f1_last": f1,
+    "auc_last": auc,
+    "pr_auc_last": pr_auc,
+    "gmean_macro_last": g_mean,
+    "fpr_last": far,
+    "training_time_total": round(training_time_total, 3)
+}
+
+    results_dir = os.path.join(args.SAVE_FOLDER, f"run_prototypical_{args.n_exps}", args.attack_type)
+    os.makedirs(results_dir, exist_ok=True)
+
+    df = pd.DataFrame([row])
+    save_path = os.path.join(results_dir, f"run_last_model_{args.n_runs}.csv")
+    df.to_csv(save_path, index=False)
+
+    print(f"Saved results to: {save_path}")
 
     print("\n" + "=" * 45)
     print("Results:")
